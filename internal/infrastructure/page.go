6d8b7c12f28010d1b0faf9e7248952583404bccd
Dmitry Osetrov <guise322@ya.ru>
2023-07-16 14:32:51 +0300

    Write a requester in the infrastructure layer to get a page with gold rate

---
+++ internal/infrastructure/page.go
@@ -0,0 +1,30 @@
+package infrastructure
+
+import (
+	"GoldRateGetter/internal/entities"
+	"io"
+	"net/http"
+)
+
+// TODO: add a config and get the url value
+const goldRateUrl = "https://investzoloto.ru/gold-sber-oms/"
+
+type Requester struct{}
+
+func (r Requester) RequestPage() entities.Response {
+	resp, err := http.Get(goldRateUrl)
+	if err != nil {
+		//TODO: appropriate error handling and logging
+		return entities.Response{Page: nil}
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		//TODO: appropriate error handling and logging
+		return entities.Response{Page: nil}
+	}
+
+	sb := string(data)
+
+	return entities.Response{Page: &sb}
+}
