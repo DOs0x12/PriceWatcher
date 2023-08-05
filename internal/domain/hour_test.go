f6a71cd46d3dcf0ae0f2db386498c4ea9ba30b07
Dmitry Osetrov <guise322@ya.ru>
2023-08-05 16:55:44 +0300

    Write tests for the hour validation

---
+++ internal/domain/hour_test.go
@@ -0,0 +1,27 @@
+package domain
+
+import "testing"
+
+func TestValide(t *testing.T) {
+	val := MessageHourVal{}
+
+	tHours := [4]int{11, 13, 16, 18}
+	want := false
+	for i := 0; i < len(tHours); i++ {
+		if got := val.Validate(tHours[i]); got != want {
+			t.Errorf("got %v, wanted %v", got, want)
+		}
+	}
+
+	tHour := 12
+	want = true
+	if got := val.Validate(tHour); got != want {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
+
+	tHour = 17
+	want = true
+	if got := val.Validate(tHour); got != want {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
+}
