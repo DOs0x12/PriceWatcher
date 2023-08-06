bbb066730aa8495154f6bf2f3e70dbbc07077223
Dmitry Osetrov <guise322@ya.ru>
2023-08-06 16:27:15 +0300

    Write tests for the serve method

--- internal/app/service_test.go
+++ internal/app/service_test.go
@@ -1,29 +1,70 @@
 package app
 
-// import (
-// 	"GoldPriceGetter/internal/entities"
-// 	"io"
-// 	"strings"
-// 	"testing"
-// )
+import (
+	"GoldPriceGetter/internal/domain"
+	"GoldPriceGetter/internal/entities"
+	"errors"
+	"io"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/agiledragon/gomonkey"
+)
+
+type testReadCloser struct {
+	Reader io.Reader
+}
+
+func (testReadCloser) Close() error                       { return nil }
+func (t testReadCloser) Read(p []byte) (n int, err error) { return t.Reader.Read(p) }
+
+type testRequester struct{}
+
+func (r testRequester) RequestPage() (entities.Response, error) {
+	s := `
+		<html>
+			<head>
+				<title>test</title>
+			</head>
+			<body>
+				<table>
+					<td> покупка: 3232.00</td>
+				</table>
+			</body>
+		</html>`
+	reader := strings.NewReader(s)
+	rc := testReadCloser{reader}
+	return entities.Response{Body: rc}, nil
+}
+
+type testSender struct{}
+
+func (s testSender) Send(price float32) error { return errors.New("HOHO") }
+
+func TestServe(t *testing.T) {
+	serv := GoldPriceService{
+		req:    testRequester{},
+		sender: testSender{},
+		ext:    domain.PriceExtractor{},
+		val:    domain.MessageHourVal{}}
+	patches := gomonkey.ApplyFunc(time.Time.Hour, func(time.Time) int {
+		return 12
+	})
+	defer patches.Reset()
+	var want error = nil
+	got := serv.serve()
+
+	if want != got {
+		t.Errorf("got error: %v", got)
+	}
+
+}
 
 // type testProcessor struct{}
 
 // func (p testProcessor) Process(page *string) float32 {
 // 	return 55.55
-// }
-
-// type testRequester struct{}
-
-// func (r testRequester) RequestPage() entities.Response {
-// 	reader := io. strings.NewReader("test55.55")
-// 	return entities.Response{Body: reader}
-// }
-
-// type testSender struct{}
-
-// func (s testSender) Send(price float32) {
-
 // }
 
 // func TestHandleGoldPrice(t *testing.T) {
