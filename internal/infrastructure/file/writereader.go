20cb5fa9610b4a954861089000a8ba623d573bb3
Dmitry Osetrov <guise322@ya.ru>
2023-09-09 19:22:44 +0300

    Add reading the last price from a file

--- internal/infrastructure/file/writereader.go
+++ internal/infrastructure/file/writereader.go
@@ -1,7 +1,11 @@
 package file
 
 import (
+	"encoding/binary"
+	"errors"
 	"fmt"
+	"io"
+	"math"
 	"os"
 	"strconv"
 )
@@ -30,5 +34,34 @@ func Write(price float32) error {
 }
 
 func Read() (float32, error) {
-	return 0.0, nil
+	file, err := os.Open(fileName)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return 0.0, nil
+		}
+
+		return 0.0, err
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	buf := make([]byte, 1024)
+	for {
+		n, err := file.Read(buf)
+		if err != nil && err != io.EOF {
+			return 0.0, fmt.Errorf("cannot read a price from the file %v: %v", fileName, err)
+		}
+		if n == 0 {
+			break
+		}
+	}
+
+	bits := binary.LittleEndian.Uint32(buf)
+	pr := math.Float32frombits(bits)
+
+	return pr, nil
 }
