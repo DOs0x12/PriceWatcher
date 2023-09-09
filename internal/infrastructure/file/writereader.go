552b6551f77bb3d9ef6a3e2bcc27f5c4faf31d80
Dmitry Osetrov <guise322@ya.ru>
2023-09-09 19:00:03 +0300

    Add writing the last price to a file

--- internal/infrastructure/file/writereader.go
+++ internal/infrastructure/file/writereader.go
@@ -1,8 +1,31 @@
 package file
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var fileName = "last_price.txt"
+
 type WriteReader struct{}
 
 func Write(price float32) error {
+	file, err := os.Create(fileName)
+	if err != nil {
+		return fmt.Errorf("cannot create a file: %v", err)
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	if _, err := file.WriteString(strconv.FormatFloat(float64(price), 'f', 2, 32)); err != nil {
+		return fmt.Errorf("cannot write a price to the file %v: %v", fileName, err)
+	}
+
 	return nil
 }
 
