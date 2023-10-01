c265d508f6885b0ed47cfd847368e1cccf8c0991
Dmitry Osetrov <guise322@ya.ru>
2023-10-01 18:44:49 +0300

    Amend the creator to create a price service

--- internal/app/creator.go
+++ internal/app/creator.go
@@ -4,16 +4,14 @@ import (
 	"PriceWatcher/internal/app/clock"
 	"PriceWatcher/internal/app/price"
 	"PriceWatcher/internal/app/price/bank"
+	marketplaceService "PriceWatcher/internal/app/price/marketplace"
 	"PriceWatcher/internal/domain/message"
 	"PriceWatcher/internal/domain/price/analyser"
 	"PriceWatcher/internal/domain/price/extractor"
-	"PriceWatcher/internal/entities/config"
 	infraFile "PriceWatcher/internal/infrastructure/file"
 	bankReq "PriceWatcher/internal/infrastructure/requester/bank"
 	"PriceWatcher/internal/infrastructure/requester/marketplace"
 	"PriceWatcher/internal/interfaces/configer"
-	"PriceWatcher/internal/interfaces/file"
-	interReq "PriceWatcher/internal/interfaces/requester"
 	interSend "PriceWatcher/internal/interfaces/sender"
 	"fmt"
 	"strings"
@@ -29,107 +27,74 @@ func NewPriceService(
 		return nil, err
 	}
 
-	req, err := createRequester(config)
-	if err != nil {
-		return nil, err
-	}
-
 	priceType := strings.ToLower(config.PriceType)
-	marketplaceType := strings.ToLower(config.Marketplace)
-
-	ext := createPriceExtractor(priceType, marketplaceType)
-	wr := createWriteReader(priceType)
-
-	analyser, err := createAnalyser(config.PriceType)
-	if err != nil {
-		return nil, err
-	}
-
-	bankType := "bank"
 	var priceService price.PriceService
+	bankPriceType := "bank"
+	marketplacePriceType := "marketplace"
 
-	if strings.ToLower(config.PriceType) == bankType {
-		priceService = bank.NewService(req, ext, val, clock.RealClock{})
+	if priceType == bankPriceType {
+		priceService = createBankPriceService()
 	}
 
-	crt := PriceService{
-		req:          req,
+	if priceType == marketplacePriceType {
+		marketplaceType := strings.ToLower(config.Marketplace)
+		priceService = createMarketplacePriceService(marketplaceType)
+	}
+
+	if priceService == nil {
+		return nil, fmt.Errorf("a price service is not created from the price type %v", priceType)
+	}
+
+	service := PriceService{
 		sender:       sender,
-		ext:          ext,
-		val:          val,
-		analyser:     analyser,
-		wr:           wr,
 		conf:         conf,
 		priceService: priceService,
 	}
 
-	return &crt, err
+	return &service, err
 }
 
-func createRequester(conf config.Config) (interReq.Requester, error) {
-	priceType := strings.ToLower(conf.PriceType)
+func createBankPriceService() price.PriceService {
+	req := bankReq.BankRequester{}
+	ext := createBankExtractor()
+	val := message.MessageHourVal{}
 
-	switch priceType {
-	case "bank":
-		return bankReq.BankRequester{}, nil
-	case "marketplace":
-		return marketplace.MarketplaceRequester{}, nil
-	default:
-		return nil, fmt.Errorf("have the unknown price type: %v", conf.PriceType)
+	return bank.NewService(req, ext, val, clock.RealClock{})
+}
+
+func createMarketplacePriceService(marketplaceType string) price.PriceService {
+	wr := infraFile.WriteReader{}
+	req := marketplace.MarketplaceRequester{}
+	ext := createMarketplaceExtractor(marketplaceType)
+	analyser := analyser.MarketplaceAnalyser{}
+
+	return marketplaceService.NewService(wr, req, ext, analyser)
+}
+
+func createMarketplaceExtractor(marketplaceType string) extractor.Extractor {
+	var pageReg, tag string
+
+	wbType := "wb"
+	ozonType := "ozon"
+
+	marketplaceTypeInLowers := strings.ToLower(marketplaceType)
+
+	if marketplaceTypeInLowers == wbType {
+		pageReg = "([0-9])*(\u00a0)*([0-9])*(\u00a0)[₽]"
+		tag = "ins"
 	}
-}
 
-func createPriceExtractor(priceType, marketplaceType string) extractor.Extractor {
-	pageReg, priceReg, tag := getSearchData(priceType, marketplaceType)
-
-	return extractor.New(pageReg, priceReg, tag)
-}
-
-func getSearchData(priceType, marketplaceType string) (pageReg, priceReg, tag string) {
-	switch priceType {
-	case "bank":
-		pageReg = `(^ покупка: [0-9]{4,5}\.[0-9][0-9])`
-		tag = "td"
-		return pageReg, priceReg, tag
-	case "marketplace":
-		if marketplaceType == "wb" {
-			pageReg = "([0-9])*(\u00a0)*([0-9])*(\u00a0)[₽]"
-			tag = "ins"
-
-			return pageReg, priceReg, tag
-		}
-
+	if marketplaceTypeInLowers == ozonType {
 		pageReg = "([0-9])*(\u2009)*([0-9])*(\u2009)[₽]"
 		tag = "span"
-
-		return pageReg, priceReg, tag
-	default:
-		return "", "", ""
 	}
+
+	return extractor.New(pageReg, tag)
 }
 
-func createAnalyser(priceType string) (analyser.Analyser, error) {
-	pType := strings.ToLower(priceType)
+func createBankExtractor() extractor.Extractor {
+	pageReg := `(^ покупка: [0-9]{4,5}\.[0-9][0-9])`
+	tag := "td"
 
-	switch pType {
-	case "bank":
-		return nil, nil
-	case "marketplace":
-		return analyser.MarketplaceAnalyser{}, nil
-	default:
-		return nil, fmt.Errorf("have the unknown price type: %v", pType)
-	}
-}
-
-func createWriteReader(priceType string) file.WriteReader {
-	pType := strings.ToLower(priceType)
-
-	switch pType {
-	case "bank":
-		return nil
-	case "marketplace":
-		return infraFile.WriteReader{}
-	default:
-		return nil
-	}
+	return extractor.New(pageReg, tag)
 }
