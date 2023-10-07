10af8f9d6db7d777b039a67f95b379a627309d35
Dmitry Osetrov <guise322@ya.ru>
2023-10-07 20:05:59 +0300

    Add tests with checking method calls into the bank service

---
+++ internal/app/price/bank/service_test.go
@@ -0,0 +1,69 @@
+package bank
+
+import (
+	"PriceWatcher/internal/entities/config"
+	"PriceWatcher/internal/entities/page"
+	"io"
+	"testing"
+	"time"
+)
+
+type testConfiger struct{}
+
+func (testConfiger) GetConfig() (config.Config, error) {
+	return config.Config{}, nil
+}
+
+var testNow time.Time
+
+type testClock struct{}
+
+func (testClock) Now() time.Time                         { return testNow }
+func (testClock) After(d time.Duration) <-chan time.Time { return time.After(d) }
+
+var (
+	reqCall bool
+	extCall bool
+	valCall bool
+)
+
+type reqWithCall struct{}
+
+func (reqWithCall) RequestPage(url string) (page.Response, error) {
+	reqCall = true
+
+	return page.Response{}, nil
+}
+
+type extWithCall struct{}
+
+func (extWithCall) ExtractPrice(body io.Reader) (float32, error) {
+	extCall = true
+
+	return 0.0, nil
+}
+
+type valWithCall struct{}
+
+func (valWithCall) Validate(hour int, sendHours []int) bool {
+	valCall = true
+
+	return true
+}
+
+func TestServePrice(t *testing.T) {
+	serv := NewService(reqWithCall{}, extWithCall{}, valWithCall{}, testClock{})
+
+	config, _ := testConfiger{}.GetConfig()
+	serv.ServePrice(config)
+
+	if !reqCall {
+		t.Error("The method for requesting a page is not called in the app layer")
+	}
+	if !extCall {
+		t.Error("The method for extracting a price is not called in the app layer")
+	}
+	if !valCall {
+		t.Error("The method for validating the price is not called in the app layer")
+	}
+}
