4709d14eaaa1dda09cefa3dd3632df49135e6c3a
Dmitry Osetrov <guise322@ya.ru>
2023-10-28 13:55:21 +0300

    Add the logic to compute waiting time for the marketplace service

--- internal/app/price/marketplace/waiting.go
+++ internal/app/price/marketplace/waiting.go
@@ -5,8 +5,23 @@ import (
 	"time"
 )
 
-func getWaitTime() time.Duration {
-	base := 20
+func getWaitTime(now time.Time) time.Duration {
+	curMinutes := now.Minute()
+	callPeriod := 30
+	var callMinutes int
+
+	if curMinutes < callPeriod {
+		callMinutes = callPeriod - curMinutes
+	} else {
+		callMinutes = callPeriod - (curMinutes - callPeriod)
+	}
+
+	callTime := getCallTimeFromMinutes(now, callMinutes)
 	variation := 10
-	return custTime.GetWaitTimeInMinutes(base, variation)
+
+	return custTime.GetWaitDurWithRandomComp(now, callTime, variation)
+}
+
+func getCallTimeFromMinutes(now time.Time, minutes int) time.Time {
+	return time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), minutes, 0, 0, now.Location())
 }
