2a3cb1c7e3733804ca12ab651892596da88e79e1
Dmitry Osetrov <guise322@ya.ru>
2023-11-06 18:26:57 +0300

    Add the test for getting a waiting duration with a random component

---
+++ internal/app/price/time/waiting_test.go
@@ -0,0 +1,57 @@
+package time
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestGetWaitDurWithRandomComp(t *testing.T) {
+	now := time.Now()
+	testHour := 5
+	testCallHour := testHour + 1
+	testNow := time.Date(now.Year(), now.Month(), now.Day(), testHour, 0, 0, 0, now.Location())
+	callTime := time.Date(now.Year(), now.Month(), now.Day(), testCallHour, 0, 0, 0, now.Location())
+	randDur, _ := time.ParseDuration("0h")
+
+	got := GetWaitDurWithRandomComp(testNow, callTime, randDur)
+
+	subHours := testCallHour - testHour
+	want, _ := time.ParseDuration(fmt.Sprintf("%vh", subHours))
+	if want != got {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+
+	got = GetWaitDurWithRandomComp(testNow, testNow, randDur)
+
+	want, _ = time.ParseDuration(fmt.Sprintf("%vh", 0))
+	if want != got {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+
+	got = GetWaitDurWithRandomComp(callTime, testNow, randDur)
+
+	want, _ = time.ParseDuration(fmt.Sprintf("%vh", 0))
+	if want != got {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+
+	randDur, _ = time.ParseDuration("2h")
+
+	got = GetWaitDurWithRandomComp(testNow, callTime, randDur)
+
+	want, _ = time.ParseDuration(fmt.Sprintf("%vh", subHours))
+	if want != got {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+
+	randMinutes := 30
+	randDur, _ = time.ParseDuration(fmt.Sprintf("%vm", randMinutes))
+
+	got = GetWaitDurWithRandomComp(testNow, callTime, randDur)
+
+	want, _ = time.ParseDuration(fmt.Sprintf("%vm", randMinutes))
+	if want != got {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+}
