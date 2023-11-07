6b77a30f0bc5506613e612a676c36e4467051be6
Dmitry Osetrov <guise322@ya.ru>
2023-11-07 10:43:53 +0300

    Fix getting call time in the part of getting 0 after processing a marketplace price

--- internal/app/price/marketplace/call_test.go
+++ internal/app/price/marketplace/call_test.go
@@ -24,7 +24,8 @@ func TestGetCallTime(t *testing.T) {
 
 	got = getCallTime(testNow)
 
-	want = time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), testMinute, 0, 0, now.Location())
+	wantMinute = 2 * testMinute
+	want = time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), wantMinute, 0, 0, now.Location())
 
 	if want.Compare(got) != 0 {
 		t.Errorf("Got %v, wanted %v", got, want)
