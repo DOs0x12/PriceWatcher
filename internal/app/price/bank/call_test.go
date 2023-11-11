5104e24ce3f26bb2491f4c10786e4307bf1abe83
Dmitry Osetrov <guise322@ya.ru>
2023-11-11 18:40:28 +0300

    Add a test to the bank call test

--- internal/app/price/bank/call_test.go
+++ internal/app/price/bank/call_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestGetCallTime(t *testing.T) {
 	now := time.Now()
-	testHour := 5
+	testHour := 14
 	testNow := time.Date(now.Year(), now.Month(), now.Day(), testHour, 1, 0, 0, now.Location())
 	callHours := []int{15, 16}
 
@@ -31,6 +31,17 @@ func TestGetCallTime(t *testing.T) {
 	}
 
 	testHour = 16
+	testNow = time.Date(now.Year(), now.Month(), now.Day(), testHour, 0, 0, 0, now.Location())
+
+	got = getCallTime(testNow, callHours)
+
+	want = time.Date(now.Year(), now.Month(), now.Day()+1, callHours[0], 0, 0, 0, now.Location())
+
+	if want.Compare(got) != 0 {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+
+	testHour = 17
 	testNow = time.Date(now.Year(), now.Month(), now.Day(), testHour, 0, 0, 0, now.Location())
 
 	got = getCallTime(testNow, callHours)
