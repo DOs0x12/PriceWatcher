1f8cfe6f5dfb5d2a0b43defac169eabb3da99f5f
Dmitry Osetrov <guise322@ya.ru>
2023-11-11 20:21:23 +0300

    Place the logic of serving item prices to a separate function

--- internal/app/price/marketplace/service.go
+++ internal/app/price/marketplace/service.go
@@ -71,41 +71,18 @@ func (s Service) ServePrice() (message, subject string, err error) {
 	priceType := capitalize(s.conf.PriceType)
 	sub := fmt.Sprintf("Цена на товар %v", priceType)
 	messages := make([]string, 0)
+	cnt := 0
 
 	for k, v := range itemPrices {
+		cnt++
 
-		response, err := s.req.RequestPage(v)
+		err := s.serveItemPrice(curPrices, &messages, k, v)
 		if err != nil {
-			return "", "", fmt.Errorf("cannot get a page with the current price: %w", err)
+			return "", "", err
 		}
 
-		price, err := s.ext.ExtractPrice(response.Body)
-		if err != nil {
-			return "", "", fmt.Errorf("cannot extract the price from the body: %w", err)
-		}
-
-		changed, up, amount := s.analyser.AnalysePrice(price, float32(curPrices[k]))
-
-		if changed && !up {
-			msg := fmt.Sprintf("Цена на %v %v уменьшилась на %.2fр. Текущая цена: %.2fр\n", k, itemPrices[k], amount, price)
-			messages = append(messages, msg)
-
-			curPrices[k] = float64(price)
-
-			logrus.Info("The item price has been changed. A report is sended")
-
-			continue
-		}
-
-		if curPrices[k] == 0.0 {
-			curPrices[k] = float64(price)
-		}
-
-		logrus.Info("The item price has been not changed")
-
-		if len(itemPrices) > 1 {
-			dur := time.Duration(60+rand.Intn(120)) * time.Second
-			time.Sleep(dur)
+		if len(itemPrices) > cnt {
+			waitNextCallWithRand()
 		}
 	}
 
@@ -134,4 +111,45 @@ func (s Service) GetWaitTime(now time.Time) time.Duration {
 
 func (Service) WaitPerStart(now time.Time) (time.Duration, error) {
 	return waitPerStart(now)
+}
+
+func waitNextCallWithRand() {
+	permanentComp := 60
+	randComp := 120
+
+	dur := time.Duration(permanentComp+rand.Intn(randComp)) * time.Second
+	time.Sleep(dur)
+}
+
+func (s Service) serveItemPrice(curPrices map[string]float64, messages *[]string, name, address string) error {
+	response, err := s.req.RequestPage(address)
+	if err != nil {
+		return fmt.Errorf("cannot get a page with the current price: %w", err)
+	}
+
+	price, err := s.ext.ExtractPrice(response.Body)
+	if err != nil {
+		return fmt.Errorf("cannot extract the price from the body: %w", err)
+	}
+
+	changed, up, amount := s.analyser.AnalysePrice(price, float32(curPrices[name]))
+
+	if changed && !up {
+		msg := fmt.Sprintf("Цена на %v %v уменьшилась на %.2fр. Текущая цена: %.2fр\n", name, address, amount, price)
+		*messages = append(*messages, msg)
+
+		curPrices[name] = float64(price)
+
+		logrus.Info("The item price has been changed. A report is sended")
+
+		return nil
+	}
+
+	if curPrices[name] == 0.0 {
+		curPrices[name] = float64(price)
+	}
+
+	logrus.Info("The item price has been not changed")
+
+	return nil
 }
