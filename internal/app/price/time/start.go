2a99219c2025680f7635f36fafe6167d66b75db3
Dmitry Osetrov <guise322@ya.ru>
2023-11-26 17:42:11 +0300

    Amend the code of getting the next period start

--- internal/app/price/time/start.go
+++ internal/app/price/time/start.go
@@ -13,18 +13,17 @@ const (
 
 func PerStartDur(now time.Time, nt NearestTime) time.Duration {
 	if nt == Hour {
-		tarTime := time.Date(now.Year(), now.Month(), now.Day(), now.Hour()+1, 0, 0, 0, now.Location())
+		starTime := time.Date(now.Year(), now.Month(), now.Day(), now.Hour()+1, 0, 0, 0, now.Location())
 
-		return tarTime.Sub(now)
+		return starTime.Sub(now)
 	}
 
 	halfHour := 30
-	tarTime := time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), halfHour, 0, 0, now.Location())
+	starTime := time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), halfHour, 0, 0, now.Location())
 
 	if now.Minute() > halfHour {
-		tarTime = tarTime.Add(time.Duration(halfHour) * time.Minute)
+		starTime = starTime.Add(time.Duration(halfHour) * time.Minute)
 	}
 
-	return tarTime.Sub(now)
-
+	return starTime.Sub(now)
 }
