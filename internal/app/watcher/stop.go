5e940700b7d372b9a66a496ee071e3b6864ae877
Dmitry Osetrov <guise322@ya.ru>
2023-12-02 17:40:00 +0300

    Add a process of waiting when all jobs is done

---
+++ internal/app/watcher/stop.go
@@ -0,0 +1,18 @@
+package watcher
+
+import (
+	"context"
+
+	"github.com/sirupsen/logrus"
+)
+
+func waithJobs(ctx context.Context, jobNames <-chan string, jobCount int) {
+	<-ctx.Done()
+
+	for i := 0; i < jobCount; i++ {
+		jobName := <-jobNames
+		logrus.Infof("The job %v is done", jobName)
+	}
+
+	logrus.Infof("All jobs is done")
+}
