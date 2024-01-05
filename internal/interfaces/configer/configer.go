245e4653ec9c64361a0bdc676553ac386e20a6a4
Dmitry Osetrov <guise322@ya.ru>
2024-01-05 19:06:12 +0300

    Add the base for getting a service config to update config before service processing

--- internal/interfaces/configer/configer.go
+++ internal/interfaces/configer/configer.go
@@ -4,6 +4,8 @@ import "PriceWatcher/internal/entities/config"
 
 type Configer interface {
 	GetConfig() (config.Config, error)
+	GetMarketplaceConfig(name string) (config.ServiceConf, error)
+	GetBankConfig(name string) (config.ServiceConf, error)
 	AddItemToWatch(address, name, priceType string) error
 	RemoveItemFromWatching(address, name, priceType string) error
 }
