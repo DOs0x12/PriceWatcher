1c5fada01383f7a35d9a9f2f4a44e88f7e0a0b00
Dmitry Osetrov <osetrovdv0x12@gmail.com>
2024-05-25 18:35:20 +0300

    Get the current with a command

--- internal/telebot/command/price/current.go
+++ internal/telebot/command/price/current.go
@@ -1,5 +1,13 @@
 package price
 
+import (
+	"PriceWatcher/internal/bank"
+	"PriceWatcher/internal/config"
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+)
+
 type CurrentPriceComm struct{}
 
 func NewPriceCommand() CurrentPriceComm {
@@ -7,5 +15,23 @@ func NewPriceCommand() CurrentPriceComm {
 }
 
 func (c CurrentPriceComm) GetCurrentPrices() string {
-	return ""
+	configer := GetConfiger()
+	conf, err := configer.GetConfig()
+	if err != nil {
+		logrus.Error("%w", err)
+	}
+
+	bankService := bank.NewService(bank.BankRequester{}, bank.NewPriceExtractor(`([0-9])*(&nbsp;)([0-9])*,([0-9])*`, "div"), conf)
+	msg, _, err := bankService.ServePrice()
+	if err != nil {
+		return fmt.Sprintf("error: %v", err)
+	}
+
+	return msg
+}
+
+func GetConfiger() config.Configer {
+	configPath := "config.yml"
+
+	return config.NewConfiger(configPath)
 }
