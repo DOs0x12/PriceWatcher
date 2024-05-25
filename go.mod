959166e2906db9f681755d1ca25a08a4cd2566a1
Dmitry Osetrov <osetrovdv0x12@gmail.com>
2024-05-25 18:12:36 +0300

    Use the rod to get the page data

--- go.mod
+++ go.mod
@@ -9,6 +9,16 @@ require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 require golang.org/x/exp v0.0.0-20240103183307-be819d1f06fc
 
 require (
+	github.com/go-rod/rod v0.116.0 // indirect
+	github.com/go-rod/stealth v0.4.9 // indirect
+	github.com/ysmood/fetchup v0.2.3 // indirect
+	github.com/ysmood/goob v0.4.0 // indirect
+	github.com/ysmood/got v0.34.1 // indirect
+	github.com/ysmood/gson v0.7.3 // indirect
+	github.com/ysmood/leakless v0.8.0 // indirect
+)
+
+require (
 	github.com/sirupsen/logrus v1.9.3
 	golang.org/x/sys v0.15.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1
--- internal/bank/page.go
+++ internal/bank/page.go
@@ -1,18 +1,32 @@
 package bank
 
 import (
-	"PriceWatcher/internal/entities/page"
+	pageEnt "PriceWatcher/internal/entities/page"
 	"fmt"
-	"net/http"
+	"strings"
+	"time"
+
+	"github.com/go-rod/rod"
+	"github.com/go-rod/stealth"
 )
 
 type BankRequester struct{}
 
-func (r BankRequester) RequestPage(url string) (page.Response, error) {
-	resp, err := http.Get(url)
+func (r BankRequester) RequestPage(url string) (pageEnt.Response, error) {
+	browser := rod.New().Timeout(time.Minute).MustConnect()
+	defer browser.MustClose()
+
+	page := stealth.MustPage(browser)
+	page.MustNavigate("https://www.sberbank.ru/ru/quotes/metalbeznal")
+	time.Sleep(20 * time.Second)
+	page.HTML()
+
+	html, err := page.HTML()
 	if err != nil {
-		return page.Response{Body: nil}, fmt.Errorf("cannot get the data from the address: %v", err)
+		return pageEnt.Response{Body: nil}, fmt.Errorf("cannot get the data from the address: %v", err)
 	}
 
-	return page.Response{Body: resp.Body}, nil
+	respReader := strings.NewReader(html)
+
+	return pageEnt.Response{Body: respReader}, nil
 }
