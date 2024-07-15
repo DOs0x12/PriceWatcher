ff4f7f5656671bb3813d74b6e068f17d619e01f6
Dmitry Osetrov <osetrovdv0x12@gmail.com>
2024-07-15 13:30:46 +0300

    Remove commented code

--- internal/app/service.go
+++ internal/app/service.go
@@ -77,15 +77,6 @@ func serveWithLogs(serv bank.Service) (string, string) {
 	return msg, sub
 }
 
-// func sendReportWithLogs(sender sender.Sender, msg, sub, servName string, email config.Email) {
-// 	err := sender.Send(msg, sub, email)
-// 	if err != nil {
-// 		logrus.Errorf("%v: cannot send the report: %v", servName, err)
-// 	}
-
-// 	logrus.Info(servName + ": a report is sended")
-// }
-
 func perStartWithLogs(serv bank.Service, now time.Time) time.Duration {
 	dur := serv.PerStartDur(now)
 	logrus.Infof("Waiting the start of the next period %v", dur)
@@ -99,72 +90,3 @@ func getWaitTimeWithLogs(serv bank.Service, now time.Time) time.Duration {
 
 	return dur
 }
-
-// func ServeWatchers(ctx context.Context,
-// 	wg *sync.WaitGroup,
-// 	configer configer.Configer,
-// 	sen sender.Sender,
-// 	wr file.WriteReader,
-// 	restart <-chan interface{}) {
-// 	defer wg.Done()
-
-// 	jobDone := make(chan interface{})
-// 	cancel, jobCnt, err := startJobs(configer, sen, wr, jobDone)
-// 	if err != nil {
-// 		logrus.Error(err)
-
-// 		return
-// 	}
-
-// 	for {
-// 		select {
-// 		case <-ctx.Done():
-// 			cancel()
-// 			WaithAllJobsDone(jobDone, jobCnt)
-
-// 			return
-// 		case <-restart:
-// 			logrus.Infof("Restart the watcher jobs")
-// 			cancel()
-// 			WaithAllJobsDone(jobDone, jobCnt)
-// 			cancel, jobCnt, err = startJobs(configer, sen, wr, jobDone)
-// 			if err != nil {
-// 				logrus.Error(err)
-
-// 				return
-// 			}
-// 		}
-// 	}
-// }
-
-// func startJobs(configer configer.Configer,
-// 	sen sender.Sender,
-// 	wr file.WriteReader,
-// 	jobDone chan<- interface{}) (context.CancelFunc, int, error) {
-// 	config, err := configer.GetConfig()
-// 	if err != nil {
-// 		return nil, 0, fmt.Errorf("can not get the config data: %v", err)
-// 	}
-
-// 	services := config.Services
-// 	jobCtx, cancel := context.WithCancel(context.Background())
-
-// 	for _, s := range services {
-// 		serv, err := price.NewPriceService(s, configer, wr)
-// 		if err != nil {
-// 			logrus.Errorf("%v: can not create a watcher service: %v", s.PriceType, err)
-
-// 			continue
-// 		}
-
-// 		go watch(jobCtx, serv, sen, s.Email, jobDone)
-// 	}
-
-// 	return cancel, len(config.Services), nil
-// }
-
-// func WaithAllJobsDone(jobDone <-chan interface{}, jobCnt int) {
-// 	for i := 0; i < jobCnt; i++ {
-// 		<-jobDone
-// 	}
-// }
