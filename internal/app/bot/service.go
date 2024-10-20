9cc1a38140b1231ebe9e65c76457b1db81a52d26
Dmitry Osetrov <osetrovdv0x12@gmail.com>
2024-10-20 18:53:47 +0300

    Add processing the bot messages

--- internal/app/bot/service.go
+++ internal/app/bot/service.go
@@ -1,26 +1,52 @@
 package telebot
 
 import (
-	"PriceWatcher/internal/entities/subscribing"
-	"PriceWatcher/internal/interfaces/bot"
+	"PriceWatcher/internal/entities/bot"
+	"PriceWatcher/internal/interfaces/broker"
 	"context"
-	"fmt"
 	"sync"
+
+	"github.com/sirupsen/logrus"
 )
 
 func Start(ctx context.Context,
 	wg *sync.WaitGroup,
-	bot bot.Worker,
-	subscribers *subscribing.Subscribers) error {
-	if err := bot.Start(ctx); err != nil {
-		return fmt.Errorf("can not start the bot: %v", err)
+	broker broker.Worker,
+	commands []bot.Command) error {
+	msgChan, err := broker.Start(ctx)
+	if err != nil {
+		return err
 	}
+
+	go processeMessages(ctx, wg, broker, msgChan, commands)
 
 	go func() {
 		<-ctx.Done()
-		bot.Stop()
+		if err := broker.Stop(); err != nil {
+			logrus.Errorln("An error occurs at stopping the message worker: ", err)
+		}
 		wg.Done()
 	}()
 
 	return nil
+}
+
+func processeMessages(ctx context.Context,
+	wg *sync.WaitGroup,
+	broker broker.Worker,
+	msgChan <-chan bot.Message,
+	commands []bot.Command) {
+	for {
+		select {
+		case <-ctx.Done():
+			wg.Done()
+		case msg := <-msgChan:
+			for _, cmd := range commands {
+				if cmd.Name == msg.Command {
+					result := cmd.Action(msg)
+					broker.SendMessage(ctx, result, msg.ChatID)
+				}
+			}
+		}
+	}
 }
