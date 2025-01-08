3cbfffc15e389506e7940b8f113bd79ecf47fb32
Dmitry Osetrov <osetrovdv0x12@gmail.com>
2025-01-08 16:48:43 +0300

    Correct the names of commands

--- internal/app/bot/command/creator.go
+++ internal/app/bot/command/creator.go
@@ -9,7 +9,7 @@ import (
 func CreateSubCommand(mu *sync.Mutex, subs *subscribing.Subscribers) bot.Command {
 	subCom := newSubCommand(mu, subs)
 	return bot.Command{
-		Name:        "/start",
+		Name:        "start",
 		Description: "Start getting messages of the current gold price ",
 		Action:      subCom.subscribeUser,
 	}
@@ -18,7 +18,7 @@ func CreateSubCommand(mu *sync.Mutex, subs *subscribing.Subscribers) bot.Command
 func CreateUnsubCommand(mu *sync.Mutex, subs *subscribing.Subscribers) bot.Command {
 	unsubCom := newUnsubCommand(mu, subs)
 	return bot.Command{
-		Name:        "/stop",
+		Name:        "stop",
 		Description: "Stop getting notifications about the current gold price ",
 		Action:      unsubCom.unsubscribeUser,
 	}
